944b60d84f2034ea4bd78820fea2862263ed1d74
Jeroen Sijsling <jeroen.sijsling@uni-ulm.de>
2020-04-16 16:44:28 +0200

    Still more verifications

--- examples/prym-calculation/transf-prop.m
+++ examples/prym-calculation/transf-prop.m
@@ -2,9 +2,9 @@
 
 SetVerbose("EndoCheck", 0);
 
-F<la> := RationalFunctionField(Rationals());
+F<a11,a12,a21,a22> := RationalFunctionField(Rationals(), 4);
 K<g2,g1,g0,h2,h1,h0> := RationalFunctionField(F, 6);
-S<x,y,z> := PolynomialRing(K, 3);
+S<y,z> := PolynomialRing(K, 2);
 R<t> := PolynomialRing(K);
 
 h := h2*y^2 + h1*y*z + h0*z^2;
@@ -22,16 +22,21 @@ a1 := B[1,1]; b1 := B[1,2]; c1 := B[1,3];
 a2 := B[2,1]; b2 := B[2,2]; c2 := B[2,3];
 a3 := B[3,1]; b3 := B[3,2]; c3 := B[3,3];
 
-a := a1 + 2*a2*t + a3*t^2;
-b := b1 + 2*b2*t + b3*t^2;
-c := c1 + 2*c2*t + c3*t^2;
+a := a1*z^2 + 2*a2*y*z + a3*y^2;
+b := b1*z^2 + 2*b2*y*z + b3*y^2;
+c := c1*z^2 + 2*c2*y*z + c3*y^2;
 
 p0 := Determinant(A)^2*b*(b^2 - a*c);
 
-
-h := Evaluate(h, [x, y + la*z, z]);
-g := Evaluate(g, [x, y + la*z, z]);
-f := Evaluate(f, [x, y + la*z, z]);
+/*
+h := Evaluate(h, [y + la*z, z]);
+g := Evaluate(g, [y + la*z, z]);
+f := Evaluate(f, [y + la*z, z]);
+*/
+ev1 := [a11*y + a12*z, a21*y + a22*z];
+h := Evaluate(h, ev1);
+g := Evaluate(g, ev1);
+f := Evaluate(f, ev1);
 
 h2 := MonomialCoefficient(h, y^2); h1 := MonomialCoefficient(h, y*z); h0 := MonomialCoefficient(h, z^2);
 g2 := MonomialCoefficient(g, y^2); g1 := MonomialCoefficient(g, y*z); g0 := MonomialCoefficient(g, z^2);
@@ -44,12 +49,12 @@ a1 := B[1,1]; b1 := B[1,2]; c1 := B[1,3];
 a2 := B[2,1]; b2 := B[2,2]; c2 := B[2,3];
 a3 := B[3,1]; b3 := B[3,2]; c3 := B[3,3];
 
-a := a1 + 2*a2*t + a3*t^2;
-b := b1 + 2*b2*t + b3*t^2;
-c := c1 + 2*c2*t + c3*t^2;
+a := a1*z^2 + 2*a2*y*z + a3*y^2;
+b := b1*z^2 + 2*b2*y*z + b3*y^2;
+c := c1*z^2 + 2*c2*y*z + c3*y^2;
 
 p := Determinant(A)^2*b*(b^2 - a*c);
-p := Evaluate(p, t + la);
 
 print "";
-print p0 eq p;
+ev2 := [a11*y - a12*z, -a21*y + a22*z];
+print Evaluate(p0, ev2) eq p;
